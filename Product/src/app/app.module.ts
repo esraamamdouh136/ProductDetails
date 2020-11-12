d9748787ab7346d3f0f1484ecd787a0a6846f039
esraa.mamdouh136 <esraa.mamdouh136@gmail.com>
2020-11-13 00:29:08 +0200

    Add bootstrap jquery popper.js And Create Components

--- Product/src/app/app.module.ts
+++ Product/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
